425bdba4b06b58d9c1766afa59ce0cd975b390f0
Ricardo Alvim <ricardo.alvim@comparegames.com.br>
2026-02-19 19:43:02 -0300

    feat: badge basic strucuture

---
+++ src/shared/enum/badge.enum.ts
@@ -0,0 +1,19 @@
+export enum Badge {
+    // Performance Badges
+    Immortal = 'Immortal',               // 0 deaths, >0 frags
+    Rambo = 'Rambo',                     // 5+ kills in 60 seconds
+    Unstoppable = 'Unstoppable',         // 10+ kill streak
+    Flawless = 'Flawless',               // 10+ frags with 0 deaths
+    Comeback = 'Comeback',               // Reverse 5+ frag deficit
+
+    // Weapon Mastery Badges
+    RifleKing = 'RifleKing',             // 80%+ kills with single weapon
+    Arsenal = 'Arsenal',                 // 3+ different weapons used effectively
+
+    // Team Badges
+    Protector = 'Protector',             // High impact on teammates
+
+    // Rare Badges
+    Perfect = 'Perfect',                 // Win (most kills) with 0 deaths
+    Blitz = 'Blitz',                     // 7+ kills in 30 seconds
+}
