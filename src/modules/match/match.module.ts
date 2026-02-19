34f103a0bbdae1a9f2523a66ce7088fdbfcb5348
Ricardo Alvim <ricardo.alvim@comparegames.com.br>
2026-02-19 18:49:34 -0300

    feat: added modules descriptions

--- src/modules/match/match.module.ts
+++ src/modules/match/match.module.ts
@@ -5,6 +5,10 @@ import { MatchEntity, MatchSchema } from './infra/persistence/match.schema'
 import { MatchRepository } from './domain/repositories/match.repository'
 import { MatchMongooseRepository } from './infra/repositories/match-mongoose.repository'
 
+/* 
+  O MatchModule é o módulo central que orquestra toda a funcionalidade relacionada às partidas. 
+  Ele importa o esquema do Mongoose para a entidade de partida, define o repositório concreto que será usado para persistência e registra o controlador que expõe as rotas da API.
+*/
 @Module({
   imports: [MongooseModule.forFeature([{ name: MatchEntity.name, schema: MatchSchema }])],
   providers: [
@@ -15,4 +19,4 @@ import { MatchMongooseRepository } from './infra/repositories/match-mongoose.rep
   ],
   controllers: [MatchController],
 })
-export class MatchModule {}
+export class MatchModule { }
--- src/modules/ranking/ranking.module.ts
+++ src/modules/ranking/ranking.module.ts
@@ -4,6 +4,15 @@ import { GlobalRankingService } from './application/services/global-ranking.serv
 import { MatchProcessedListener } from './application/listeners/match-processed.listener'
 import { RedisCacheModule } from 'src/infra/cache/redis-cache.module'
 
+/* 
+Módulo de Ranking
+
+* - RankingController: Expõe a API REST para consultar o ranking global
+* - GlobalRankingService: Lógica de negócio para calcular e retornar o ranking global
+* - MatchProcessedListener: Ouve eventos de partidas processadas para atualizar o ranking no Redis
+* - RedisCacheModule: Módulo de cache para armazenar o ranking global atualizado
+
+*/
 @Module({
     imports: [RedisCacheModule],
     controllers: [RankingController],
