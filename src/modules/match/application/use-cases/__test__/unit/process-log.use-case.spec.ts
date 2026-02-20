d0b4c377acb61c8b346d846269ed4690d68fd6f8
Ricardo Alvim <ricardo.alvim@comparegames.com.br>
2026-02-19 21:44:09 -0300

    fix: added idepotency test

--- src/modules/match/application/use-cases/__test__/unit/process-log.use-case.spec.ts
+++ src/modules/match/application/use-cases/__test__/unit/process-log.use-case.spec.ts
@@ -67,10 +67,22 @@ describe('ProcessLogUseCase', () => {
 
     expect(repository.save).toHaveBeenCalledTimes(1)
     const [savedMatch] = repository.save.mock.calls[0]
-
     expect(savedMatch.matchId).toBe('111')
-    expect(savedMatch.winningWeapon).toBe(Weapon.M16)
-
     expect(eventEmitter.emit).toHaveBeenCalledWith('match.processed', savedMatch)
+  })
+
+  it('should skip processing if matchId already exists (idempotency)', async () => {
+    const fileBuffer = Buffer.from('23/04/2019 15:34:22 - New match 123 has started')
+    const mockMatch = { matchId: '123', players: {} }
+
+    // Usando as variáveis injetadas corretamente (parser e repository)
+    parser.parseLogContent.mockReturnValue([mockMatch as any])
+    repository.findById.mockResolvedValue(mockMatch as any)
+
+    await useCase.execute(fileBuffer)
+
+    // Verifica a idempotência: não deve salvar nem emitir evento se o ID já existe
+    expect(repository.save).not.toHaveBeenCalled()
+    expect(eventEmitter.emit).not.toHaveBeenCalled()
   })
 })
