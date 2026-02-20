61604cce075a6762ba130791a59abd9902252270
Ricardo Alvim <ricardo.alvim@comparegames.com.br>
2026-02-20 02:02:24 -0300

    fix: use-case fail import file

--- src/modules/match/application/use-cases/get-player-comparison.use-case.ts
+++ src/modules/match/application/use-cases/get-player-comparison.use-case.ts
@@ -1,20 +1,24 @@
 import { Injectable, BadRequestException } from '@nestjs/common'
 import { PlayerHeadToHeadComparison } from 'src/shared/interfaces/analytics.interface'
 import { PlayerComparisonService } from '../services/player-comparison.service'
+import { MatchRepository } from '../../domain/repositories/match.repository'
 
 @Injectable()
 export class GetPlayerComparisonUseCase {
-  constructor(private readonly comparisonService: PlayerComparisonService) {}
+  constructor(
+    private readonly matchRepository: MatchRepository,
+    private readonly comparisonService: PlayerComparisonService
+  ) { }
 
   async execute(player1: string, player2: string): Promise<PlayerHeadToHeadComparison> {
     if (!player1 || !player2) {
       throw new BadRequestException('Both player names are required')
     }
-
     if (player1 === player2) {
-      throw new BadRequestException('Cannot compare player with themselves')
+      throw new BadRequestException('Players must be different')
     }
 
-    return this.comparisonService.compareHeadToHead(player1, player2)
+    const matches = await this.matchRepository.findPlayersInteractions(player1, player2)
+    return this.comparisonService.compareHeadToHead(player1, player2, matches)
   }
 }
